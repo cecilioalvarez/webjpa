0d7f226f66981fc2402cffd9f16a4b09f74eb754
Formacion <Formacion@5CD9494ZLTC>
2020-04-08 12:06:22 +0200

    borrar

--- web/src/main/java/es/avalon/web/controladores/ServletLibros.java
+++ web/src/main/java/es/avalon/web/controladores/ServletLibros.java
@@ -32,6 +32,27 @@ public class ServletLibros extends HttpServlet {
 
 		// hay algun tipo de accion
 		if (accion != null) {
+
+			if (accion.equals("formularioInsertar")) {
+				despachador = request.getRequestDispatcher("libros2/formularioInsertar.jsp");
+			}
+			// Borrar
+			// Accion=borrar
+			else if (accion.equals("borrar")) {
+				// Recepcionar
+				String isbn = request.getParameter("isbn");
+
+				// Borrar
+				Libro milibro = new Libro(isbn);
+				repositorio.borrar(milibro);
+
+				// Cargar nuevo listado
+				List<Libro> listaLibros = repositorio.buscarTodos();
+				request.setAttribute("listaLibros", listaLibros);
+				// Redirigir
+				despachador = request.getRequestDispatcher("libros2/listaLibros.jsp");
+			}
+
 		} else {
 
 			List<Libro> listaLibros = new ArrayList<Libro>();
