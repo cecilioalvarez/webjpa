1311e5526a0b60958b9498a821a89314ccc6c65e
inesrollan <inesrmh@gmail.com>
2020-04-08 12:09:36 +0200

    nueva ines

--- web/src/main/java/es/avalon/web/controladores/ServletLibros.java
+++ web/src/main/java/es/avalon/web/controladores/ServletLibros.java
@@ -32,6 +32,55 @@ public class ServletLibros extends HttpServlet {
 
 		// hay algun tipo de accion
 		if (accion != null) {
+
+			if (accion.equals("formularioInsertar")) {
+
+				despachador = request.getRequestDispatcher("libros2/formularioInsertar.jsp");
+
+			} else if (accion.equals("insertar")) {
+
+				// accion=insertar
+
+				// recepcionar
+				String isbn = request.getParameter("isbn");
+				String titulo = request.getParameter("titulo");
+				String autor = request.getParameter("autor");
+				int precio = Integer.parseInt(request.getParameter("precio"));
+				String categoria = request.getParameter("categoria");
+
+				// insertar
+				Libro milibro = new Libro(isbn, titulo, autor, precio);
+				repositorio.insertar(milibro);
+
+				// cargar el nuevo listado
+				List<Libro> listaLibros = repositorio.buscarTodos();
+				request.setAttribute("listaLibros", listaLibros);
+
+				// redirigir
+				despachador = request.getRequestDispatcher("libros2/listaLibros.jsp");
+
+			} else if (accion.equals("borrar")){
+				
+				//accion=borrar
+				
+				//recepcionar
+				String isbn = request.getParameter("isbn");
+
+				//borrar
+				Libro milibro = new Libro(isbn);
+				repositorio.borrar(milibro);
+				
+				// cargar el nuevo listado				
+				List<Libro> listaLibros=repositorio.buscarTodos();
+				request.setAttribute("listaLibros", listaLibros);
+
+				// redirigir
+				despachador = request.getRequestDispatcher("libros2/listaLibros.jsp");
+				
+			}else {
+
+			}
+
 		} else {
 
 			List<Libro> listaLibros = new ArrayList<Libro>();
