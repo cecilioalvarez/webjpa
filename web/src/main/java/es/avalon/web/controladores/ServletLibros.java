5783432d90c3cae81b5a6670d1a83c2db48aefd0
Formacion <Formacion@5CD9494ZLKC>
2020-04-13 12:30:26 +0200

    moises web bien

--- web/src/main/java/es/avalon/web/controladores/ServletLibros.java
+++ web/src/main/java/es/avalon/web/controladores/ServletLibros.java
@@ -3,26 +3,19 @@ package es.avalon.web.controladores;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import es.avalon.dominio.Categoria;
 import es.avalon.dominio.Libro;
 import es.avalon.repositorios.CategoriaRepository;
 import es.avalon.repositorios.LibroRepository;
-import es.avalon.repositorios.jdbc.LibroRepositoryJDBC;
 import es.avalon.repositorios.jpa.CategoriaRepositoryJPA;
 import es.avalon.repositorios.jpa.LibroRepositoryJPA;
 
-/**
- * aportan informacion clave al servidor
- */
-// http://localhost:8080/avalon2020web1/ServletLibros
 @WebServlet("/ServletLibros")
 public class ServletLibros extends HttpServlet {
 
@@ -34,48 +27,72 @@ public class ServletLibros extends HttpServlet {
 		LibroRepository repositorio = new LibroRepositoryJPA();
 		CategoriaRepository repositorioCategoria = new CategoriaRepositoryJPA();
 
-		// hay algun tipo de accion
 		if (accion != null) {
-			
-			
+
 			if (accion.equals("formularioInsertar")) {
 
 				despachador = request.getRequestDispatcher("libros2/formularioInsertar.jsp");
-			}else {
 
-				
+			} else if (accion.equals("insertar")) {
+
 				String isbn = request.getParameter("isbn");
 				String titulo = request.getParameter("titulo");
 				String autor = request.getParameter("autor");
 				int precio = Integer.parseInt(request.getParameter("precio"));
 				String categoria = request.getParameter("categoria");
 
-			
 				Libro milibro = new Libro(isbn, titulo, autor, precio);
-				
-				//uso el nuevo repositorio para buscar una categoria
-				Categoria micategoria= repositorioCategoria.buscarPorNombre(categoria);
-				//asigno al libro la categoria que necesita
+				Categoria micategoria = repositorioCategoria.buscarPorNombre(categoria);
 				milibro.setCategoria(micategoria);
-				
-				//salvo el libro
 				repositorio.insertar(milibro);
-				
-				
+
 				List<Libro> listaLibros = repositorio.buscarTodos();
 				request.setAttribute("listaLibros", listaLibros);
-				// redirigir
 				despachador = request.getRequestDispatcher("libros2/listaLibros.jsp");
+
+			} else if (accion.equals("borrar")) {
+
+				String isbn = request.getParameter("isbn");
+				Libro libro = new Libro(isbn);
+				repositorio.borrar(libro);
+
+				List<Libro> listaLibros = repositorio.buscarTodos();
+				request.setAttribute("listaLibros", listaLibros);
+				despachador = request.getRequestDispatcher("libros2/listaLibros.jsp");
+
+			} else if (accion.equals("detalle")) {
+
+				String isbn = request.getParameter("isbn");
+				Libro libro = repositorio.buscarPorISBN(isbn);
+				request.setAttribute("libro", libro);
+				despachador = request.getRequestDispatcher("libros2/detalle.jsp");
+
+			} else if (accion.equals("editar")) {
+
+				String isbn = request.getParameter("isbn");
+				Libro libro = repositorio.buscarPorISBN(isbn);
+				request.setAttribute("libro", libro);
+				despachador = request.getRequestDispatcher("libros2/editar.jsp");
+
+			} else if (accion.equals("salvar")) {
+
+				String isbn = request.getParameter("isbn");
+				String titulo = request.getParameter("titulo");
+				String autor = request.getParameter("autor");
+				int precio = Integer.parseInt(request.getParameter("precio"));
+				String categoria = request.getParameter("categoria");
+
+				Libro milibro = new Libro(isbn, titulo, autor, precio);
+				Categoria micategoria = repositorioCategoria.buscarPorNombre(categoria);
+				milibro.setCategoria(micategoria);
+				repositorio.salvar(milibro);
+
+				List<Libro> listaLibros = repositorio.buscarTodos();
+				request.setAttribute("listaLibros", listaLibros);
+				despachador = request.getRequestDispatcher("libros2/listaLibros.jsp");
+
 			}
-			
-			
-			
-			
-			
-			
-			
-			
-			
+
 		} else {
 
 			List<Libro> listaLibros = new ArrayList<Libro>();
@@ -86,7 +103,6 @@ public class ServletLibros extends HttpServlet {
 			request.setAttribute("listaLibros", listaLibros);
 		}
 
-		// redirige al jsp
 		despachador.forward(request, response);
 	}
 
