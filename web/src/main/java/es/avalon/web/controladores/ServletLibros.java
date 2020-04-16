a1c25cadf4abfe2e198574cd15fc0e1753499841
Formacion <Formacion@5CD9494ZLTC>
2020-04-16 10:14:10 +0200

    ultimos cambios

--- web/src/main/java/es/avalon/web/controladores/ServletLibros.java
+++ web/src/main/java/es/avalon/web/controladores/ServletLibros.java
@@ -18,6 +18,7 @@ import es.avalon.repositorios.LibroRepository;
 import es.avalon.repositorios.jdbc.LibroRepositoryJDBC;
 import es.avalon.repositorios.jpa.CategoriaRepositoryJPA;
 import es.avalon.repositorios.jpa.LibroRepositoryJPA;
+import es.avalon.web.servicios.ServicioLibros;
 
 /**
  * aportan informacion clave al servidor
@@ -31,8 +32,11 @@ public class ServletLibros extends HttpServlet {
 
 		RequestDispatcher despachador = null;
 		String accion = request.getParameter("accion");
-		LibroRepository repositorio = new LibroRepositoryJPA();
-		CategoriaRepository repositorioCategoria = new CategoriaRepositoryJPA();
+		ServicioLibros servicio=new ServicioLibros();
+		
+		//LibroRepository repositorio = new LibroRepositoryJPA();
+		
+		//CategoriaRepository repositorioCategoria = new CategoriaRepositoryJPA();
 
 		// hay algun tipo de accion
 		if (accion != null) {
@@ -48,10 +52,10 @@ public class ServletLibros extends HttpServlet {
 				String ordenarPor = request.getParameter("ordenarPor");
 				if (ordenarPor.contentEquals("titulo")) {
 
-					List<Libro> listaLibros = repositorio.buscarTodosOrdenadosPorTitulo();		
+					List<Libro> listaLibros = servicio.buscarLibroTodosOrdenadosPorTitulo();		
 					request.setAttribute("listaLibros", listaLibros);
 				} else {
-					List<Libro> listaLibros = repositorio.buscarTodosOrdenadosPorAutor();
+					List<Libro> listaLibros = servicio.buscarLibroTodosOrdenadosPorAutor();
 					request.setAttribute("listaLibros", listaLibros);
 
 				}
@@ -67,10 +71,10 @@ public class ServletLibros extends HttpServlet {
 
 				// Borrar
 				Libro milibro = new Libro(isbn);
-				repositorio.borrar(milibro);
+				servicio.borrarLibro(milibro);
 
 				// Cargar nuevo listado
-				List<Libro> listaLibros = repositorio.buscarTodos();
+				List<Libro> listaLibros = servicio.buscarLibroTodos();
 				request.setAttribute("listaLibros", listaLibros);
 				// Redirigir
 				despachador = request.getRequestDispatcher("libros2/listaLibros.jsp");
@@ -80,7 +84,7 @@ public class ServletLibros extends HttpServlet {
 			// Accion=detalle
 			else if (accion.contentEquals("detalle")) {
 				String isbn = request.getParameter("isbn");
-				Libro libro = repositorio.buscarPorISBN(isbn);
+				Libro libro = servicio.buscarLibroPorISBN(isbn);
 				request.setAttribute("libro", libro);
 				despachador = request.getRequestDispatcher("libros2/detalle.jsp");
 
@@ -90,7 +94,7 @@ public class ServletLibros extends HttpServlet {
 			// Accion=editar
 			else if (accion.contentEquals("editar")) {
 				String isbn = request.getParameter("isbn");
-				Libro libro = repositorio.buscarPorISBN(isbn);
+				Libro libro = servicio.buscarLibroPorISBN(isbn);
 				request.setAttribute("libro", libro);
 				despachador = request.getRequestDispatcher("libros2/editar.jsp");
 
@@ -106,13 +110,13 @@ public class ServletLibros extends HttpServlet {
 
 				// Salvar
 				Libro milibro = new Libro(isbn, titulo, autor, precio);
-				Categoria objetoCategoria = repositorioCategoria.buscarPorNombre(categoria);
+				Categoria objetoCategoria = servicio.buscarCategoriaPorNombre(categoria);
 				milibro.setCategoria(objetoCategoria);
 				// Salvo el libro
-				repositorio.salvar(milibro);
+				servicio.salvarLibro(milibro);
 
 				// Cargar nuevo listado
-				List<Libro> listaLibros = repositorio.buscarTodos();
+				List<Libro> listaLibros = servicio.buscarLibroTodos();
 				request.setAttribute("listaLibros", listaLibros);
 				// Redirigir
 				despachador = request.getRequestDispatcher("libros2/listaLibros.jsp");
@@ -131,14 +135,14 @@ public class ServletLibros extends HttpServlet {
 				// Insertar
 				Libro milibro = new Libro(isbn, titulo, autor, precio);
 				// Uso el nuevo repositorio para buscar una categoria
-				Categoria micategoria = repositorioCategoria.buscarPorNombre(categoria);
+				Categoria micategoria = servicio.buscarCategoriaPorNombre(categoria);
 				// Asigno al libro la categoria que necesita
 				milibro.setCategoria(micategoria);
 				// Salvo el libro
-				repositorio.insertar(milibro);
+				servicio.insertarLibro(milibro);
 
 				// Cargar nuevo listado
-				List<Libro> listaLibros = repositorio.buscarTodos();
+				List<Libro> listaLibros = servicio.buscarLibroTodos();
 				request.setAttribute("listaLibros", listaLibros);
 				// Redirigir
 				despachador = request.getRequestDispatcher("libros2/listaLibros.jsp");
@@ -148,7 +152,7 @@ public class ServletLibros extends HttpServlet {
 
 			List<Libro> listaLibros = new ArrayList<Libro>();
 
-			listaLibros = repositorio.buscarTodos();
+			listaLibros = servicio.buscarLibroTodos();
 
 			despachador = request.getRequestDispatcher("libros2/listaLibros.jsp");
 			request.setAttribute("listaLibros", listaLibros);
